29b2f2b8679ff04133cdc076dc21b79ede8751ad
Angelos Bouklis <angelbouklis.official@gmail.com>
2024-03-13 20:10:45 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/uni/news-and-announcements")
 def news_and_announcements() -> Response:
-    return Response(content=get_cs_feed(), media_type="application/rss+xml", charset="utf-8")
+    return Response(content=get_cs_feed(), media_type="application/rss+xml")
